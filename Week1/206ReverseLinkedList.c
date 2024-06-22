a788e5e83e708e9e59232597af5e97a8914a719f
sharma-aak <sharma.aak@northeastern.edu>
2024-06-22 14:45:16 -0400

    added files

---
+++ Week1/206ReverseLinkedList.c
@@ -0,0 +1,11 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *reverseList(struct ListNode *head)
+{
+    struct ListNode *prev = NULL;
+}
