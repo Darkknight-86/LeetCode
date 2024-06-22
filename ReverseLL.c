317e9cd1f8f4963755f82bea29ec0dd41d266e5e
Darkknight-86 <bewick.j@northeastern.edu>
2024-06-22 14:37:54 -0400

    test3

--- ReverseLL.c
+++ ReverseLL.c
@@ -1,4 +1,11 @@
-sdf;
-kljhasdf;
-lkajsdf;
-lasdkjf
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *reverseList(struct ListNode *head)
+{
+    struct ListNode *prev = NULL;
+}
